c1813ad6322dc3ef07b01974b9ec08806c40066b
fubu <fubu.hades@gmail.com>
2026-05-31 10:01:13 +0200

    Update scan-range.sh

--- scan-range.sh
+++ scan-range.sh
@@ -3,6 +3,6 @@
 # usage: sudo ./scan-range.sh 200
 # will scan 200.*.*.* for port 5900
 masscan -p 5900 $!.0.0.0/8 -oG vnc --rate=100000
-cat vnc | awk '/Host:/ { print $4 }' >> vnc-$1
+cat vnc | awk '/Host:/ { print $4 }' >> vnc-$1;rm -rf vnc
 nmap --script open-curtains.nse -n -Pn -p 5900 -iL vnc-$1
 
