1a24c5e161fd81b90a9fcd4db92432db977974b1
fubu <fubu.hades@gmail.com>
2026-05-31 05:56:24 +0200

    Update scan-range.sh to continuously scan for VNC
    Modified scan-range.sh to run masscan in a loop and log results.

--- scan-range.sh
+++ scan-range.sh
@@ -2,6 +2,7 @@
 # will get vnc screenshots for your chosen range
 # usage: sudo ./scan-range.sh 200
 # will scan 200.*.*.* for port 5900
-masscan -p 5900 $1.0.0.0/8 -oG vnc --rate=100000
+while sleep 20; do masscan -p 5900 $1.0.0.0/8 -oG vnc --rate=100000
 cat vnc | awk '/Host:/ { print $4 }' >> vnc-$1
 nmap --script open-curtains.nse -n -Pn -p 5900 -iL vnc-$1
+;done
